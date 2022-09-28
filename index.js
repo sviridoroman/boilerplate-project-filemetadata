75f15552f46750d3ea160e34e27c526e40008501
sviridoroman <sviridoroman@gmail.com>
2022-09-28 23:24:21 +0300

    add File Metadata Microservice

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+let multer = require('multer')
 
 var app = express();
 
@@ -11,7 +12,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', multer({storage: multer.memoryStorage()}).single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
 
 
 const port = process.env.PORT || 3000;
